cb35bef795c928918fcec935c32a0372cd6e3978
Vlad717 <58781719+Vlad717@users.noreply.github.com>
2020-01-23 19:44:56 +0200

    Add brands slider

--- src/js/brands.js
+++ src/js/brands.js
@@ -3,11 +3,13 @@ $('.slick-slider').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    arrows: false
 });
 
 
 $('.popular-products-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    dots: true,
+    dots: false,
+    arrows: false,
 });
